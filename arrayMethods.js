40a56d09c6545a4957674c1457e67e31e78280dd
ktakesue <ktakesue@aol.com>
2017-12-19 11:33:25 -1000

    the no spaces were bothering me lol

--- arrayMethods.js
+++ arrayMethods.js
@@ -34,7 +34,7 @@ mixedNums.sort();
 
 mixedWords.sort();
 
-var notFruit = fruitCollection.splice(1,3);
+var notFruit = fruitCollection.splice(1, 3);
 
 gemBox.splice(5, 2);
 gemBox.splice(6, 4);
@@ -43,7 +43,7 @@ upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8);
 
 brownOnly.splice(1, 5, 'brown', 'brown','brown');
 
-var orderedValuesNewLength = orderedValues.unshift(1,2,3);
+var orderedValuesNewLength = orderedValues.unshift(1, 2, 3);
 
 var randomThingsArray = genericNumberArray.concat(colors);
 
